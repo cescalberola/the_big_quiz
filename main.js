42d970767162e8753e85a45deddb0fe6f2a6cfa2
Francesc Alberola <f.alberola@gmail.com>
2023-10-14 16:07:12 +0200

    test_funciona/localstorage

--- main.js
+++ main.js
@@ -1,99 +1,123 @@
-let currentQuestionIndex = 0;
-let questions = [];
-let resultMessage = null;
+let questionCounter = 0;
+        let questions = [];
+        let resultMessage = null;
+        let isRenderingNextQuestion = false;
 
-function getQuestions() {
-  axios.get('https://opentdb.com/api.php?amount=10&type=multiple')
-    .then(res => {
-      const data = res.data;
-      questions = data.results;
-      renderQuestion();
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
+        // Al comienzo del script, verifica si hay un progreso almacenado en el localStorage
+        const savedProgress = localStorage.getItem('quizProgress');
+        if (savedProgress) {
+            questionCounter = parseInt(savedProgress, 10); // Restaura el progreso guardado
+        }
 
-function renderQuestion() {
-  if (currentQuestionIndex < questions.length) {
-    const question = questions[currentQuestionIndex];
+        function getQuestions() {
+            axios.get('https://opentdb.com/api.php?amount=10&type=multiple')
+                .then(res => {
+                    const data = res.data;
+                    questions = data.results;
+                    renderQuestion();
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
 
-    const questionHeading = document.getElementById('question-heading');
-    const questionText = document.getElementById('question-text');
-    const answerButtons = document.querySelectorAll('#answer-buttons .button');
+        function renderNextQuestion() {
+  // Evitar ejecución duplicada
+  if (isRenderingNextQuestion) {
+    return;
+  }
 
-    questionHeading.textContent = `Pregunta ${currentQuestionIndex + 1}:`;
-    questionText.textContent = question.question;
+  isRenderingNextQuestion = true;
 
-    const answers = shuffleArray(question.incorrect_answers.concat(question.correct_answer));
+  setTimeout(() => {
+    isRenderingNextQuestion = false; // Permitir que se llame nuevamente después del setTimeout
+  }, 1000);
 
-    answerButtons.forEach((button, i) => {
-      button.textContent = answers[i];
-      button.removeAttribute('disabled');
-      button.setAttribute('data-answer', answers[i]);
+  questionCounter++;
 
-      button.addEventListener('click', () => {
-        const selectedAnswer = button.getAttribute('data-answer');
-        const correctAnswer = question.correct_answer;
-        checkAnswer(selectedAnswer, correctAnswer);
-      });
-    });
-
-    if (resultMessage) {
-      resultMessage.remove();
-    }
+  if (questionCounter < questions.length) {
+    renderQuestion();
   } else {
     showFinalMessage();
   }
 }
 
-function checkAnswer(selectedAnswer, correctAnswer) {
-  if (resultMessage) {
-    resultMessage.remove();
-  }
+        function renderQuestion() {
+            if (questionCounter < questions.length) {
+                const question = questions[questionCounter];
 
-  resultMessage = document.createElement('p');
-  if (selectedAnswer === correctAnswer) {
-    resultMessage.textContent = '¡Felicitaciones! La respuesta es correcta.';
-    resultMessage.style.color = 'green';
-  } else {
-    resultMessage.textContent = 'Ánimo, la respuesta es incorrecta.';
-    resultMessage.style.color = 'red';
-  }
+                const questionHeading = document.getElementById('question-heading');
+                const questionText = document.getElementById('question-text');
+                const answerButtons = document.querySelectorAll('#answer-buttons .button');
 
-  const quizPage = document.getElementById('quiz-page');
+                questionHeading.textContent = `Pregunta ${questionCounter + 1}:`;
+                questionText.textContent = question.question;
 
-  quizPage.appendChild(resultMessage);
+                const answers = shuffleArray(question.incorrect_answers.concat(question.correct_answer));
 
-  const buttons = quizPage.querySelectorAll('button');
-  buttons.forEach(button => {
-    button.disabled = true;
-  });
+                answerButtons.forEach((button, i) => {
+                    button.textContent = answers[i];
+                    button.removeAttribute('disabled');
+                    button.setAttribute('data-answer', answers[i]);
 
-  setTimeout(() => {
-    currentQuestionIndex++;
+                    button.addEventListener('click', () => {
+                        const selectedAnswer = button.getAttribute('data-answer');
+                        const correctAnswer = question.correct_answer;
+                        checkAnswer(selectedAnswer, correctAnswer);
+                    });
+                });
 
-    if (currentQuestionIndex < questions.length) {
-      renderQuestion();
-    } else {
-      showFinalMessage();
-    }
-  }, 3000);
-}
+                if (resultMessage) {
+                    resultMessage.remove();
+                }
+            } else {
+                showFinalMessage();
+                // Limpia el progreso del cuestionario del localStorage una vez completado
+                localStorage.removeItem('quizProgress');
+            }
+        }
 
+        function checkAnswer(selectedAnswer, correctAnswer) {
+            if (resultMessage) {
+                resultMessage.remove();
+            }
 
-function shuffleArray(array) {
-  const shuffled = [...array];
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled;
-}
+            resultMessage = document.createElement('p');
+            if (selectedAnswer === correctAnswer) {
+                resultMessage.textContent = '¡Felicitaciones! La respuesta es correcta.';
+                resultMessage.style.color = 'green';
+            } else {
+                resultMessage.textContent = 'Ánimo, la respuesta es incorrecta.';
+                resultMessage.style.color = 'red';
+            }
 
-function showFinalMessage() {
-  const quizPage = document.getElementById('quiz-page');
-  quizPage.innerHTML = '<h3>¡Felicidades, has completado el cuestionario!</h3>';
-}
+            const quizPage = document.getElementById('quiz-page');
 
-document.addEventListener('DOMContentLoaded', getQuestions);
+            quizPage.appendChild(resultMessage);
+
+            const buttons = quizPage.querySelectorAll('button');
+            buttons.forEach(button => {
+                button.disabled = true;
+            });
+
+            setTimeout(() => {
+                renderNextQuestion();
+            }, 1000);
+        }
+
+        function shuffleArray(array) {
+            const shuffled = [...array];
+            for (let i = shuffled.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+            }
+            return shuffled;
+        }
+
+        function showFinalMessage() {
+            const quizPage = document.getElementById('quiz-page');
+            quizPage.innerHTML = '<h3>¡Felicidades, has completado el gran Quiz!</h3>';
+            localStorage.removeItem('quizProgress');
+        }
+
+        document.addEventListener('DOMContentLoaded', getQuestions);
